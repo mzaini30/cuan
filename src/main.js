39a26e11127607016962ef6acce488be1ecb571b
Zen <muhzaini30@gmail.com>
2022-11-15 13:08:24 +0800

    Oke

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import App from "./App.svelte";
 import "npm:@unocss/reset/tailwind.css";
-import "npm:uno.css";
+import "uno.css";
 
 const app = new App({
   target: document.getElementById("app"),
