428b639d1124783fb64bee2890bbeac4c76b222b
Zen <muhzaini30@gmail.com>
2022-11-15 11:35:36 +0800

    Oke

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,18 @@ import { svelte } from "npm:@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    exclude: [
+      "svelte/animate",
+      "svelte/easing",
+      "svelte/internal",
+      "svelte/motion",
+      "svelte/store",
+      "svelte/transition",
+      "svelte",
+      "svelte-hmr/runtime/hot-api-esm.js",
+      "svelte-hmr/runtime/proxy-adapter-dom.js",
+      "svelte-hmr",
+    ],
+  },
 });
